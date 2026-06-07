39536dd2b50ab290f92158792513905175ee8c4f
MohanKrishna <mohankrishnamohankrishna28@gmail.com>
2026-06-07 11:16:48 +0530

    created API for social account creation

---
+++ server/middleware/authMiddlewares.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../Models/user.js";
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export const protect = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+      req.user = User.findById(decoded.id).select("-password");
+      next();
+    } catch (error: any) {
+      res
+        .status(401)
+        .json({ message: error.message || "Not authorized, Token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no Token " });
+  }
+};
